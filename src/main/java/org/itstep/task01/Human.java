a099098aaa17e502c3328b20356091307fee4499
Kiril <jolymod96@gmail.com>
2023-01-25 23:19:29 +0200

    Realized City functionality

--- src/main/java/org/itstep/task01/Human.java
+++ src/main/java/org/itstep/task01/Human.java
@@ -1,4 +1,90 @@
 package org.itstep.task01;
 
+import java.util.Date;
+
 public class Human {
+    // Fields
+    private String fullName;
+    private String dateOfBirth;
+    private String phone;
+    private String city;
+    private String country;
+    private String address;
+
+    // Constructors
+    public Human()
+    {
+        this(new String(), new String(), new String(), new String(), new String(), new String());
+    }
+
+    public Human(String fullName, String dateOfBirth, String phone, String city, String country, String address)
+    {
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.city = city;
+        this.country = country;
+        this.address = address;
+    }
+
+    // Methods
+    public String getFullName()
+    {
+        return this.fullName;
+    }
+
+    public void setFullName(String fullName)
+    {
+        this.fullName = fullName;
+    }
+
+    public String getDateOfBirth()
+    {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth)
+    {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhone()
+    {
+        return this.phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getCity()
+    {
+        return this.city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getCountry()
+    {
+        return this.country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getAddress()
+    {
+        return this.address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
 }
--- src/main/java/org/itstep/task02/City.java
+++ src/main/java/org/itstep/task02/City.java
@@ -1,4 +1,71 @@
 package org.itstep.task02;
 
 public class City {
+    private String name;
+    private String region;
+    private String country;
+    private int inhabitants;
+    private String index;
+    private String code;
+
+    public City() {
+        this(new String(), new String(), new String(), 0, new String(), new String());
+    }
+
+    public City(String name, String region, String country, int inhabitants, String index, String code) {
+        this.name = name;
+        this.region = region;
+        this.country = country;
+        this.inhabitants = inhabitants;
+        this.index = index;
+        this.code = code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return this.region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getInhabitants() {
+        return this.inhabitants;
+    }
+
+    public void setInhabitants(int inhabitants) {
+        this.inhabitants = inhabitants;
+    }
+
+    public String getIndex() {
+        return this.index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
