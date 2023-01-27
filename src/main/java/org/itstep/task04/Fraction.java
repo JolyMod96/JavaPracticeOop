d8115d8a61e4c388acd40d890ab918854382962c
Kiril <jolymod96@gmail.com>
2023-01-27 10:49:15 +0200

    Realized Fraction (with problem) and Money (Particularly) functionality

--- src/main/java/org/itstep/task04/Fraction.java
+++ src/main/java/org/itstep/task04/Fraction.java
@@ -20,5 +20,62 @@ public class Fraction {
         this.denominator = denominator;
     }
 
+    // TODO:Решить проблему с математическими операциями
 
+    private int GCD(int x, int y) {
+        int r = 0;
+        int a = Math.max(x, y);
+        int b = Math.min(x, y);
+        r = b;
+        while(a % b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return r;
+    }
+
+    public Fraction addition(Fraction fraction) {
+        Fraction result = new Fraction(this.numerator * fraction.denominator + fraction.numerator * this.denominator , fraction.numerator * fraction.denominator);
+        int gcd = this.GCD(Math.abs(result.numerator), Math.abs(result.denominator));
+        result.numerator /= gcd;
+        result.denominator /= gcd;
+        return result;
+    }
+
+    public Fraction subtraction(Fraction fraction) {
+        Fraction result = new Fraction(this.numerator * fraction.denominator - fraction.numerator * this.denominator , fraction.numerator * fraction.denominator);
+        int gcd = this.GCD(Math.abs(result.numerator), Math.abs(result.denominator));
+        result.numerator /= gcd;
+        result.denominator /= gcd;
+        return result;
+    }
+
+    public Fraction multiplication(Fraction fraction) {
+        Fraction result = new Fraction(this.numerator * fraction.denominator * fraction.numerator * this.denominator , fraction.numerator * fraction.denominator);
+        int gcd = this.GCD(Math.abs(result.numerator), Math.abs(result.denominator));
+        result.numerator /= gcd;
+        result.denominator /= gcd;
+        return result;
+    }
+
+    public Fraction division(Fraction fraction) {
+        Fraction result = new Fraction(this.numerator * fraction.denominator / fraction.numerator * this.denominator , fraction.numerator * fraction.denominator);
+        int gcd = this.GCD(Math.abs(result.numerator), Math.abs(result.denominator));
+        result.numerator /= gcd;
+        result.denominator /= gcd;
+        return result;
+    }
+
+    public Fraction() {
+        this(0, 1);
+    }
+
+    public Fraction(int numerator, int denominator) {
+        if(denominator == 0) {
+            throw new ArithmeticException();
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
 }
--- src/main/java/org/itstep/task05/Money.java
+++ src/main/java/org/itstep/task05/Money.java
@@ -1,4 +1,61 @@
 package org.itstep.task05;
 
 public class Money {
+    private long hryvnia;
+    private byte kopecks;
+
+    public long getHryvnia() {
+        return this.hryvnia;
+    }
+
+    public void setHryvnia(long hryvnia) {
+        this.hryvnia = hryvnia;
+    }
+
+    public byte getKopecks() {
+        return this.kopecks;
+    }
+
+    public void setKopecks(byte kopecks) {
+        this.kopecks = kopecks;
+    }
+
+    public Money() {
+        this(0L, (byte)0);
+    }
+
+    public Money(long hryvnia) {
+        this(hryvnia, (byte)0);
+    }
+
+    public Money(long hryvnia, byte kopecks) {
+        this.hryvnia = hryvnia;
+        this.kopecks = kopecks;
+    }
+
+    public void printlnBalance() {
+        System.out.println(this.hryvnia+","+this.kopecks);
+    }
+
+    // TODO:Реализовать методы арифметических операций
+
+    public Money addition(Money money) {
+        return new Money();
+    }
+
+    public Money subtraction(Money money) {
+        return new Money();
+    }
+
+    public Money division(double money) {
+        return new Money();
+    }
+
+    public Money multiply(double money) {
+        return new Money();
+    }
+
+    public boolean equals(Money money) {
+        return this.hryvnia == money.hryvnia && this.kopecks == money.kopecks;
+    }
 }
