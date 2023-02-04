a5c61e1060409a2a1a300cb4fe2c0017d893a182
JolyMod96 <jolymod96@gmail.com>
2023-02-04 12:40:31 +0200

    Realized MainString and particularly Complex functionality

--- src/main/java/org/itstep/task06/MainString.java
+++ src/main/java/org/itstep/task06/MainString.java
@@ -1,4 +1,64 @@
 package org.itstep.task06;
 
+import java.util.stream.IntStream;
+
 public class MainString {
+    private char[] chars;
+
+    public MainString() {
+        clean();
+    }
+
+    public MainString(CharSequence charSequence) {
+        int[] ch = charSequence.chars().toArray();
+        this.chars = new char[ch.length];
+        for(int i = 0; i < ch.length; i++) {
+            this.chars[i] = (char)ch[i];
+        }
+    }
+
+    public MainString(char symbol, int length) {
+        this.chars = new char[length];
+        for(int i = 0; i < length; i++) {
+            this.chars[i] = symbol;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = new String();
+        for(char ch:this.chars) {
+            str += ch;
+        }
+        return str;
+    }
+
+    public int length() {
+        return this.chars.length;
+    }
+
+    public void clean() {
+        this.chars = new char[0];
+    }
+
+    public MainString concat(MainString mainString) {
+        MainString resultMainString = new MainString();
+        resultMainString.chars = new char[this.chars.length+ mainString.chars.length];
+        for(int i = 0; i < this.chars.length; i++) {
+            resultMainString.chars[i] = this.chars[i];
+        }
+        for(int i = this.chars.length; i < resultMainString.chars.length; i++) {
+            resultMainString.chars[i] = mainString.chars[i - this.chars.length];
+        }
+        return resultMainString;
+    }
+
+    public int indexOf(int ch) {
+        for(int i = 0; i < this.chars.length; i++) {
+            if(this.chars[i] == (char)ch) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/main/java/org/itstep/task07/Complex.java
+++ src/main/java/org/itstep/task07/Complex.java
@@ -1,4 +1,23 @@
 package org.itstep.task07;
 
 public class Complex {
+    private double real;
+    private double imaginary;
+
+    public Complex() {
+        this(0, 0);
+    }
+
+    public Complex(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(getClass(obj) == Complex) {
+
+        }
+        return false;
+    }
 }
