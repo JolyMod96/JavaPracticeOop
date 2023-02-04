f9fb190f9d70ed7626cf0910354f6f6c93477a9d
Kiril <jolymod96@gmail.com>
2023-02-04 14:08:26 +0200

    Realized getters and setters for Complex class

--- src/main/java/org/itstep/task07/Complex.java
+++ src/main/java/org/itstep/task07/Complex.java
@@ -15,9 +15,42 @@ public class Complex {
 
     @Override
     public boolean equals(Object obj) {
-        if(getClass(obj) == Complex) {
-
+        if(obj == this) {
+            return true;
         }
-        return false;
+        if(!(obj instanceof Complex)) {
+            return false;
+        }
+        Complex complex = (Complex)obj;
+        return Double.compare(this.real, complex.real) == 0
+                && Double.compare(this.imaginary, complex.imaginary) == 0;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    public Complex plus(Complex complex) {
+
+    }
+
+    public Complex minus(Complex complex) {
+
+    }
+
+    public Complex times(Complex complex) {
+
     }
 }
